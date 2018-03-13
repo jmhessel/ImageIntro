ee6e78e9143f727ee04c7e741ebbe7d7d3313c91
Jack Hessel <jmhessel@gmail.com>
2018-03-12 22:39:13 -0400

    more work

--- util.py
+++ util.py
@@ -2,6 +2,15 @@ import numpy as np
 
 from PIL import Image
 
+from keras.applications.imagenet_utils import preprocess_input
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.models import Sequential, Model
+from keras.layers import Flatten
+
+import os, sys, numpy as np
+
+
 def isolate_channel(m_array, channel=0):
     '''
     Given a height, width, channel image,
@@ -39,3 +48,26 @@ def constant_color_image(r, g, b, size=100):
     m_arr[:,:,1] = g
     m_arr[:,:,2] = b
     return Image.fromarray(m_arr)
+
+
+def nearest_neighbor_search(annoy_idx, image_idx, k=10):
+    nns = annoy_idx.get_nns_by_item(image_idx, k)
+
+def load_images_for_neural_network(fnames, batch_size):
+    while True:#Keras generators need to loop forever for some reason...
+        cfns = []
+        for i, p in enumerate(fnames):
+            cfns.append(p)
+            if len(cfns) == batch_size:
+                yield load_images_keras(cfns)
+                cfns = []
+        if len(cfns) != 0:
+            yield load_images_keras(cfns)
+            cfns = []
+
+def load_images_keras(image_list):
+    images = []
+    for i in image_list:
+        c_img = np.expand_dims(image.img_to_array(image.load_img(i, target_size = (224, 224))), axis = 0)
+        images.append(c_img)
+    return preprocess_input(np.vstack(images))
